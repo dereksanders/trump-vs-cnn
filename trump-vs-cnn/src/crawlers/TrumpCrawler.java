18491963e3a9593b7f42a367b627827261c37a7f
Derek Sanders <derekrodneysanders@gmail.com>
2017-12-16 23:22:59 -0500

    Started implementing TrumpCrawler::crawl().

--- trump-vs-cnn/src/crawlers/TrumpCrawler.java
+++ trump-vs-cnn/src/crawlers/TrumpCrawler.java
@@ -1,6 +1,7 @@
 package crawlers;
 
 import java.net.URL;
+import java.util.HashMap;
 
 import main.TrumpTweet;
 
@@ -14,5 +15,20 @@ public class TrumpCrawler extends Crawler<TrumpTweet> {
 	public void crawl() {
 
 		StringBuilder rawInput = this.loadWebpageSource();
+		HashMap<String, String> features = new HashMap<>();
+
+		// To find the time of a tweet, first search for "small class="time"" and
+		// then "data-time=" to find the Unix timestamp (seconds from the epoch of
+		// 1970-01-01T00:00:00Z) enclosed in quotation marks.
+
+		String tweetTime = "";
+		features.put("time", tweetTime);
+
+		// To find the content of the tweet, first search for <div
+		// class="js-tweet-text-container"> and then between <p class=*> and </p> will
+		// be the text.
+
+		String tweetText = "";
+		features.put("text", tweetText);
 	}
 }
--- trump-vs-cnn/src/main/TrumpTweet.java
+++ trump-vs-cnn/src/main/TrumpTweet.java
@@ -1,5 +1,13 @@
 package main;
 
+import java.time.LocalDateTime;
+
 public class TrumpTweet {
 
+	private LocalDateTime time;
+	private String text;
+
+	public TrumpTweet(LocalDateTime time, String text) {
+
+	}
 }

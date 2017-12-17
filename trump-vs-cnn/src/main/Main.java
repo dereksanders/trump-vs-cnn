ce328436fe950bb70e13564c7907d62cb6c6b458
Derek Sanders <derekrodneysanders@gmail.com>
2017-12-17 17:33:22 -0500

    Added Main::writeFile().

--- trump-vs-cnn/src/main/Main.java
+++ trump-vs-cnn/src/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import crawlers.TrumpCrawler;
@@ -10,6 +13,7 @@ public class Main {
 
 	public static final boolean DEBUG = true;
 	private static final String TRUMP_TWITTER = "https://twitter.com/realdonaldtrump";
+	private static final int MAX_TWEET_LENGTH = 280;
 
 	public static void main(String[] args) {
 
@@ -22,5 +26,30 @@ public class Main {
 		}
 
 		ArrayList<TrumpTweet> tweets = trumpTweets.crawl();
+
+		StringBuilder allTweets = new StringBuilder(tweets.size() * MAX_TWEET_LENGTH);
+
+		for (TrumpTweet t : tweets) {
+
+			allTweets.append(t.toString());
+			allTweets.append("\n\n");
+		}
+
+		// Get rid of trailing endlines.
+		allTweets.substring(0, allTweets.lastIndexOf("\n\n"));
+
+		writeFile(allTweets.toString(), "trumpTweets.txt");
+	}
+
+	private static void writeFile(String text, String filename) {
+
+		try {
+
+			Files.write(Paths.get("./" + filename), text.getBytes());
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
 	}
 }

fb4a33b71e255959c4772bc8bfb6293f416034b5
Derek Sanders <derekrodneysanders@gmail.com>
2017-12-18 21:16:07 -0500

    Updated directory structure.

--- trump-vs-cnn-front/scripts/loadTweets.js
+++
@@ -1,9 +0,0 @@
-var tweetsJSON;
-
-$.getJSON('data/trumpTweets.json', function(json){
-    tweetsJSON = json;
-})
-
-var tweetsDisplay = JSON.stringify(tweetsJSON, null, 2);
-
-document.getElementById('tweets').textContent = tweetsDisplay;

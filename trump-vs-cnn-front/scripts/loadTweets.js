3efc2f175616ca95b39d0450f628f4e404ea65da
Derek Sanders <derekrodneysanders@gmail.com>
2017-12-18 19:38:40 -0500

    Updated loadTweets.js to display JSON objects.

--- trump-vs-cnn-front/scripts/loadTweets.js
+++ trump-vs-cnn-front/scripts/loadTweets.js
@@ -1,13 +1,9 @@
-function loadTweets(url) {
-    
-    var request = new XMLHttpRequest();
-    
-    request.onload = function () {
-    
-        document.getElementById('tweets').textContent = this.responseText;
-    };
-    
-    request.open('GET', url);
-    request.send();
-}
-loadTweets('https://trump-vs-cnn.000webhostapp.com/data/trumpTweets.txt');
+var tweetsJSON;
+
+$.getJSON('data/trumpTweets.json', function(json){
+    tweetsJSON = json;
+})
+
+var tweetsDisplay = JSON.stringify(tweetsJSON, null, 2);
+
+document.getElementById('tweets').textContent = tweetsDisplay;

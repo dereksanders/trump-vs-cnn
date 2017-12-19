002a3c3ffc7cfed82848038b26c519e5713a2203
Derek Sanders <derekrodneysanders@gmail.com>
2017-12-18 19:18:17 -0500

    Adjusted Main to fit new TweetCrawler and Tweet classes.

--- trump-vs-cnn/src/main/Main.java
+++ trump-vs-cnn/src/main/Main.java
@@ -7,29 +7,31 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
-import crawlers.TrumpCrawler;
+import crawlers.TweetCrawler;
 
 public class Main {
 
 	public static final boolean DEBUG = true;
+
+	private static final String TRUMP_USER = "@realDonaldTrump";
 	private static final String TRUMP_TWITTER = "https://twitter.com/realdonaldtrump";
 	private static final int MAX_TWEET_LENGTH = 280;
 
 	public static void main(String[] args) {
 
-		TrumpCrawler trumpTweets = null;
+		TweetCrawler trumpTweets = null;
 
 		try {
-			trumpTweets = new TrumpCrawler(new URL(TRUMP_TWITTER));
+			trumpTweets = new TweetCrawler(new URL(TRUMP_TWITTER), TRUMP_USER);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
 
-		ArrayList<TrumpTweet> tweets = trumpTweets.crawl();
+		ArrayList<Tweet> tweets = trumpTweets.crawl();
 
 		StringBuilder allTweets = new StringBuilder(tweets.size() * MAX_TWEET_LENGTH);
 
-		for (TrumpTweet t : tweets) {
+		for (Tweet t : tweets) {
 
 			allTweets.append(t.toString());
 			allTweets.append("\n\n");
